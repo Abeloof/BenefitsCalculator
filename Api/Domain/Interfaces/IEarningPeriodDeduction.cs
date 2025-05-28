591c0222ae84576b6ee685419d2544259e4ae646
Debian Live user <user@localhost.localdomain>
2025-05-28 04:47:18 +0000

    added interface annotations

--- Api/Domain/Interfaces/IEarningPeriodDeduction.cs
+++ Api/Domain/Interfaces/IEarningPeriodDeduction.cs
@@ -5,8 +5,29 @@ namespace Api.Domain.Interfaces;
 
 public interface IEarningPeriodDeduction
 {
+    /// <summary>
+    /// Given an employee dto, it returns ture/false to indicate earning deduction applies for a period
+    /// </summary>
+    /// <param name="employee"></param>
+    /// <returns>boolean</returns>
     bool IsDeductable(GetEmployeeDto employee);
+
+    /// <summary>
+    /// Given an earning period id and employee data, it return earning period deductions.
+    /// 
+    /// NOTE: earning period deductions are applied evenly across a given range. As such, method ignores
+    ///       earning period id but accepts the id in anticipation requirements might change. 
+    /// </summary>
+    /// <param name="employee">employee related data</param>
+    /// <param name="earningPeriodId">optional earning period id</param>
+    /// <returns></returns>
     EarningPeriodDeductionDto RetrieveDeduction(GetEmployeeDto employee, int earningPeriodId)
         => RetrieveDeduction(employee);
+
+    /// <summary>
+    /// Given an employee data, method calculates applicable deductions.
+    /// </summary>
+    /// <param name="employee">employee related data</param>
+    /// <returns></returns>
     EarningPeriodDeductionDto RetrieveDeduction(GetEmployeeDto employee);
 }
--- Api/Domain/Interfaces/IEmployeesService.cs
+++ Api/Domain/Interfaces/IEmployeesService.cs
@@ -4,6 +4,18 @@ namespace Api.Domain.Interfaces;
 
 public interface IEmployeesService
 {
+    /// <summary>
+    /// Returns all employees.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns>List of mployees</returns>
     Task<IList<GetEmployeeDto>> GetEmployeesAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Returns an employee whois employee id matchs request's id.
+    /// </summary>
+    /// <param name="id">Requests employee id</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>an employee</returns>
     Task<GetEmployeeDto?> GetEmployeeAsync(int id, CancellationToken cancellationToken = default);
 }
